e53b095420377229c393e93986f5f70eaf5dc2fd
Lauren <lauren@transmogrifier.local>
2013-03-22 18:53:49 -0700

    WIP3

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ import sqlite3
 def connect_db():
 	return sqlite3.connect("tipsy.db")
 
+
+#bob_id = new_user(db, "bob@loblaw.com", "password", "Bob Loblaw")
 def new_user(db, email, password, name):
     c = db.cursor()
     query = """INSERT INTO Users VALUES (NULL, ?, ?, ?)"""
@@ -17,7 +19,7 @@ def new_user(db, email, password, name):
 
 def authenticate(db, email, password):
     c = db.cursor()
-    query = """SELECT * from Users WHERE email=? AND password=?"""
+    query = """SELECT * FROM Users WHERE email=? AND password=?"""
     c.execute(query, (email, password))
     result = c.fetchone()
     if result:
@@ -39,7 +41,7 @@ def get_user(db, user_id):
 
 def get_task(db, task_id):
 	c = db.cursor()
-	query = """SELECT * FROM Tasks WHERE user_name=?"""
+	query = """SELECT * FROM Tasks WHERE task_id=?"""
 	result = c.fetchone()
 	if result:
 		credentials = ['id', 'title', 'created_at', 'completed_at', 'user_id']
@@ -49,14 +51,23 @@ def get_task(db, task_id):
 
 def get_tasks(db, user_id):
 	c = db.cursor()
-	query = 
-
+	query = """SELECT * FROM Tasks WHERE user_id=?"""
+	result = c.fetchone()
+	if result:
+		user_id = True
+		print "USER SPECIFIC TASKS"
+		return dict(zip(result))
+	return
+	query = """SELECT * FROM Tasks"""
+	result = c.fetchone()
+	print "ALL TASKS"
+	return dict(zip(result))
 
 
 def new_task(db, title, user_id):
 	c = db.cursor()
 	query = """INSERT INTO Tasks VALUES (NULL, ?, ?)"""
-	created_at = datetime.now()
+	created_at = NULL #datetime.now()
 	completed_at = NULL
 	result = c.execute(query, (title, user_id, created_at,
     completed_at))
@@ -66,7 +77,9 @@ def new_task(db, title, user_id):
 
 def complete_task(db, task_id):
 	c = db.cursor()
-	query = """"INSERT INTO Tasks VALUES ()"""
+	query = """INSERT INTO Tasks VALUES (?)"""
+
+
 
 ## new_task(db, title, user_id) 
 # -- Created a new task, returns the id of the newly created row. Make sure to populate the created_at field.
@@ -74,9 +87,12 @@ def complete_task(db, task_id):
 # -- Fetch a user's record based on his id. Return the user as a dictionary, like our authenticate method.
 ## complete_task(db, task_id)
 #  -- Marks a task as being complete, setting the completed_at field.
-## get_tasks(db, user_id) 
+# get_tasks(db, user_id) 
 # -- Gets all the tasks for the given user id. Returns all the tasks in the system if no user_id is given. Returns them as a list of dictionaries.
-# get_task(db, task_id)
+## get_task(db, task_id)
 #  -- Get a single task, given its id. Return the task as a dictionary as above in the authenticate method.
 
-# test where clause with select and then use UPDATE values
+
+
+
+# test with where clause with select and then use UPDATE values
---
+++ seed.py
@@ -0,0 +1,5 @@
+import model
+
+db = model.connect_db()
+user_id = model.new_user(db, "chriszf@gmail.com", "securepassword", "Christian")
+task = model.new_task(db, "Complete this task list", user_id)
